3f1b8c162331cc968ec9e24cddcea3b297d6d104
USER <mon001024@naver.com>
2025-07-08 14:44:24 +0900

    plz

---
+++ src/main/java/com/MH/to_do_list_with_dto/controller/SignupController.java
@@ -0,0 +1,51 @@
+package com.MH.to_do_list_with_dto.controller;
+
+import com.MH.to_do_list_with_dto.dto.SignupDto;
+import com.MH.to_do_list_with_dto.model.User;
+import com.MH.to_do_list_with_dto.repository.UserRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class SignupController {
+    private final UserRepository userRepository;
+
+    @GetMapping("/signup")
+    public String showSignup(Model model) {
+        model.addAttribute("signupDto", new SignupDto());
+
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String doSignup(
+            @Valid @ModelAttribute("signupDto") SignupDto signupDTO,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "signup";
+        }
+
+        if (userRepository.findByUsername(signupDTO.getUsername()) != null) {
+            model.addAttribute("error", "이미 사용 중인 아이디입니다");
+
+            return "signup";
+        }
+
+        User user = User.builder()
+                .username(signupDTO.getUsername())
+                .password(signupDTO.getPassword())
+                .build();
+        userRepository.save(user);
+
+        return "redirect:/login?registered";
+    }
+}
