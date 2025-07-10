59fade794e597880f2376928e17f97f6614336d0
USER <mon001024@naver.com>
2025-07-10 16:07:00 +0900

    plz

--- src/main/java/com/MH/to_do_list_with_dto/controller/SignupController.java
+++ src/main/java/com/MH/to_do_list_with_dto/controller/SignupController.java
@@ -26,7 +26,7 @@ public class SignupController {
 
     @PostMapping("/signup")
     public String doSignup(
-            @Valid @ModelAttribute("signupDto") SignupDto signupDTO,
+            @Valid @ModelAttribute("signupDto") SignupDto signupDto,
             BindingResult bindingResult,
             Model model
     ) {
@@ -34,15 +34,15 @@ public class SignupController {
             return "signup";
         }
 
-        if (userRepository.findByUsername(signupDTO.getUsername()) != null) {
+        if (userRepository.findByUsername(signupDto.getUsername()) != null) {
             model.addAttribute("error", "이미 사용 중인 아이디입니다");
 
             return "signup";
         }
 
         User user = User.builder()
-                .username(signupDTO.getUsername())
-                .password(signupDTO.getPassword())
+                .username(signupDto.getUsername())
+                .password(signupDto.getPassword())
                 .build();
         userRepository.save(user);
 
